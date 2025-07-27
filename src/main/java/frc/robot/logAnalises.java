f9879fdb83412eb419378d67a7f220476e6bfa57
AlmaKaplan <alma20062010@gmail.com>
2025-07-28 00:40:59 +0300

    work on gettimebetweenStates() :)

--- src/main/java/frc/robot/logAnalises.java
+++ src/main/java/frc/robot/logAnalises.java
@@ -11,33 +11,52 @@ import edu.wpi.first.wpilibj.Timer;
 public class LogAnalises {
 
     private Timer timeInCurrentStatetimer;
+    private Timer timebetweenStatestimer;
+
 
     private RobotState lastRobotState;
-    private RobotState currentRobotState;
+    private Supplier<RobotState> currentRobotState;
+    private Supplier<RobotState> firstRobotState;
+    private Supplier<RobotState> secondRobotState;
 
 
-
-    public LogAnalises(Supplier<RobotState> currentRobotState) {
+    public LogAnalises(Supplier<RobotState> currentRobotState,
+     Supplier<RobotState> firstRobotState, Supplier<RobotState> secondRobotState) {
         lastRobotState = null;
 
-        this.currentRobotState = currentRobotState.get();
+        this.currentRobotState = currentRobotState;
+        this.firstRobotState = firstRobotState;
+        this.secondRobotState = secondRobotState;
 
         timeInCurrentStatetimer = new Timer();
         timeInCurrentStatetimer.start();
+
+        timebetweenStatestimer = new Timer();
+        timebetweenStatestimer.start();
+
     }
 
     public double getTimeInCurrentState() {
         
         if (lastRobotState == null) {
-            lastRobotState = currentRobotState;
+            lastRobotState = currentRobotState.get();
         }
 
-        if(currentRobotState.equals(lastRobotState)) {
+        if(currentRobotState.get().equals(lastRobotState)) {
             return timeInCurrentStatetimer.get();
         } else {
-            lastRobotState = currentRobotState;
+            lastRobotState = currentRobotState.get();
             timeInCurrentStatetimer.restart();
             return 0;
         }
+    }
+
+    public double gettimebetweenStates() {
+        if (currentRobotState.get().equals(firstRobotState.get())) {
+            timebetweenStatestimer.restart();
+        } else if(currentRobotState.get().equals(secondRobotState.get())) {
+            return timebetweenStatestimer.get();
+        }
+        return -1;
     }
 }
