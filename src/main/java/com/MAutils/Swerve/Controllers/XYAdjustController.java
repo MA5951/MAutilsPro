4727287f1e0447141e8df4f63258d263f5bee449
GalDotan <93068735+GalDotan@users.noreply.github.com>
2025-08-02 21:39:57 +0200

    Update XYAdjustController.java

--- src/main/java/com/MAutils/Swerve/Controllers/XYAdjustController.java
+++ src/main/java/com/MAutils/Swerve/Controllers/XYAdjustController.java
@@ -3,17 +3,25 @@ package com.MAutils.Swerve.Controllers;
 
 import java.util.function.Supplier;
 
+import com.MAutils.Logger.MALog;
+import com.MAutils.Swerve.SwerveSystem;
 import com.MAutils.Swerve.Utils.SwerveController;
+import com.MAutils.Utils.ChassisSpeedsUtil;
+import com.MAutils.Utils.DriverStationUtil;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
-public class XYAdjustController extends SwerveController{
+public class XYAdjustController extends SwerveController {
 
     private PIDController xController;
     private PIDController yController;
     private Supplier<Double> xSupplier;
     private Supplier<Double> ySupplier;
+    private Supplier<Double> gyroSupplier;
+    private boolean fieldRelative = true;
 
     public XYAdjustController(PIDController xController, PIDController yController,
             Supplier<Double> xSupplier, Supplier<Double> ySupplier) {
@@ -24,39 +32,70 @@ public class XYAdjustController extends SwerveController{
         this.ySupplier = ySupplier;
     }
 
+    public XYAdjustController(SwerveSystem swerveSystem, PIDController xController, PIDController yController,
+            Supplier<Double> xSupplier, Supplier<Double> ySupplier) {
+        super("XY Adjust Controller");
+        this.xController = xController;
+        this.yController = yController;
+        this.xSupplier = xSupplier;
+        this.ySupplier = ySupplier;
+    }
+
+    public XYAdjustController withGyroSupplier(Supplier<Double> gyroSupplier) {
+        this.gyroSupplier = gyroSupplier;
+        return this;
+    }
+
+    public XYAdjustController withFieldRelative(boolean fieldRelative) {
+        this.fieldRelative = fieldRelative;
+        return this;
+    }
+
     public XYAdjustController withXYControllers(PIDController xController, PIDController yController) {
         this.xController = xController;
         this.yController = yController;
         return this;
     }
 
-    public XYAdjustController withXSuppliers(Supplier<Double> xSupplier, Supplier<Double> ySupplier) {
+    public XYAdjustController withXYSetPoint(Supplier<Double> xSupplier, Supplier<Double> ySupplier) {
         this.xSupplier = xSupplier;
         this.ySupplier = ySupplier;
         return this;
     }
 
-    public XYAdjustController withXSetPoint(double xSetPoint, double ySetPoint) {
-        xController.setSetpoint(xSetPoint);
-        yController.setSetpoint(ySetPoint);
+    public XYAdjustController withXYSetPoint(double xSetPoint, double ySetPoint) {
+        this.xSupplier = () -> xSetPoint;
+        this.ySupplier = () -> ySetPoint;
         return this;
     }
 
-    public ChassisSpeeds getSpeeds() {
+    public void updateSpeeds() {
         speeds.vxMetersPerSecond = xController.calculate(xSupplier.get());
         speeds.vyMetersPerSecond = yController.calculate(ySupplier.get());
-        logController();
-        return speeds;
+
+        if (fieldRelative && DriverStationUtil.getAlliance() == Alliance.Blue) {
+            speeds = ChassisSpeedsUtil.FromFieldToRobot(speeds, new Rotation2d(
+                    Math.toRadians((-(gyroSupplier.get())))));
+        } else if (fieldRelative) {
+            speeds = ChassisSpeedsUtil.FromFieldToRobot(speeds, new Rotation2d(
+                    Math.toRadians((-(gyroSupplier.get()) - 180))));
+        }
     }
 
     public boolean atSetpoint() {
         return xController.atSetpoint() && yController.atSetpoint();
     }
 
-    private void logController() {
-        System.out.println("Speeds: " + speeds);
-        System.out.println("X Set Point: " + xController.getSetpoint());
-        System.out.println("Y Set Point: " + yController.getSetpoint());
-        System.out.println("At Set Point: " + atSetpoint());
+    public Pose2d getSetPoint() {
+        return new Pose2d(xController.getSetpoint(), yController.getSetpoint(), new Rotation2d());
+    }
+
+    public void logController() {
+        super.logController();
+
+        MALog.log("/Subsystems/Swerve/Controllers/XYAdjustController/X Set Point", xController.getSetpoint());
+        MALog.log("/Subsystems/Swerve/Controllers/XYAdjustController/Y Set Point", yController.getSetpoint());
+        MALog.log("/Subsystems/Swerve/Controllers/XYAdjustController/X At Point", xController.atSetpoint());
+        MALog.log("/Subsystems/Swerve/Controllers/XYAdjustController/Y At Point", yController.atSetpoint());
     }
 }
