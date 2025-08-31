4ccc178ee84ff0e29b37db778575958e6063ef74
GalDotan <93068735+GalDotan@users.noreply.github.com>
2025-08-31 03:11:23 +0300

    Added support for multipule tags

--- src/main/java/com/MAutils/Simulation/Simulatables/GamePieceStateSimulation.java
+++ src/main/java/com/MAutils/Simulation/Simulatables/GamePieceStateSimulation.java
@@ -5,16 +5,15 @@ import java.util.ArrayList;
 import java.util.function.Supplier;
 
 import com.MAutils.Logger.MALog;
+import com.MAutils.Simulation.SimulationManager;
 import com.MAutils.Simulation.Utils.Simulatable;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 
 public class GamePieceStateSimulation implements Simulatable {
-    public static final GamePieceState BLANK_STATE = new GamePieceState("BLANK_STATE", () -> false,
-            () -> new Pose3d(-1, -1, -1, new Rotation3d()));
 
-    public static class GamePieceState {
+    public class GamePieceState {
         public final Supplier<Pose3d> gamePiecePoseSupplier;
         public final String stateName;
         public final Supplier<Boolean> stateEnterSupplier;
@@ -28,16 +27,20 @@ public class GamePieceStateSimulation implements Simulatable {
 
     }
 
-    private static ArrayList<GamePieceState> gamePieceStates = new ArrayList<>();
-    private static GamePieceState currentState = BLANK_STATE;
-
-    public static void addStates(GamePieceState... states) {
+    public GamePieceStateSimulation(GamePieceState... states) {
         for (GamePieceState state : states) {
             gamePieceStates.add(state);
         }
+        SimulationManager.registerSimulatable(this);
     }
 
-    public static GamePieceState getCurrentState() {
+    public final GamePieceState BLANK_STATE = new GamePieceState("BLANK_STATE", () -> false,
+            () -> new Pose3d(-1, -1, -1, new Rotation3d()));
+
+    private ArrayList<GamePieceState> gamePieceStates = new ArrayList<>();
+    private GamePieceState currentState = BLANK_STATE;
+
+    public GamePieceState getCurrentState() {
         return currentState;
     }
 
